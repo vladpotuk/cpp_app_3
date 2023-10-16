270f6dedb438e899cd4e19ad79171e2a6b9cf226
user <user@WIN-TE96RD9SUDU>
2023-10-16 23:57:57 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    system("chcp 1251");
+    system("cls");
+    std::cout << "Продам цуценят," << std::endl;
+    std::cout << "Порода: ротвейлер," << std::endl;
+    std::cout << "__" << std::endl;
+    std::cout << "Тел.: 222-22-22" << std::endl;
+    std::cout << "222          |           222        |" << std::endl;
+    std::cout << "22           |           22         |" << std::endl;
+    std::cout << "22           |           22         |" << std::endl;
+
+    return 0;
+}
